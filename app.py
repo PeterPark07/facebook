4d210c66a05b0609b9ad1889bdf67d687eb44853
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-04 01:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,31 @@ import os
 app = Flask(__name__)
 
 # Connect to MongoDB
-client = MongoClient(os.getenv('mongodb'))
-db = client['chat']
+mongo_client = MongoClient(os.getenv('mongodb'))
+db = mongo_client['chat']
 messages_collection = db['messages']
-
-def persist_message(message_text):
-    should_persist = False
-    if '/persist' in message_text:
-        should_persist = True
-        message_text = message_text.replace('/persist', '')
-    return message_text, should_persist
 
 @app.route('/')
 def index():
+    # Retrieve all messages from the database
     messages = messages_collection.find()
     return render_template('index.html', messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
+    # Get user input
     username = request.form.get('username')
-    message_text = request.form.get('message')
-
-    if username and message_text:
-        message_text, should_persist = persist_message(message_text)
+    user_message = request.form.get('message')
+    should_persist = False
+    
+    if username and user_message:
+        # Check for persistence flag in the message
+        if '/persist' in user_message:
+            should_persist = True
+            user_message = user_message.replace('/persist', '')
+        # Store the message in the database
         timestamp = datetime.utcnow()
-        new_message = {'username': username, 'message': message_text, 'timestamp': timestamp, 'persist': should_persist}
+        new_message = {'username': username, 'message': user_message, 'timestamp': timestamp, 'persist': should_persist}
         messages_collection.insert_one(new_message)
         
         return jsonify({'success': True})
@@ -40,18 +40,23 @@ def send():
 
 @app.route('/delete-chats', methods=['POST'])
 def delete_chats():
+    # Get the password for authentication
     password = request.form.get('pin')
 
     # Implement your password validation logic here
     # For simplicity, let's assume the password is 'secret'
-    if password in ['0', '1234']:
-        if password == '0':
-            messages_collection.delete_many({'persist': False})
-        elif password == '1234':
-            messages_collection.delete_many({})
+    if password == '0':
+        # Delete messages without persistence flag
+        messages_collection.delete_many({'persist': False})
         return jsonify({'success': True})
         
-    return jsonify({'success': False, 'error': 'Invalid password.'})
+    elif password == '1234':
+        # Delete all messages
+        messages_collection.delete_many({})
+        return jsonify({'success': True})
+        
+    else:
+        return jsonify({'success': False, 'error': 'Invalid password.'})
 
 if __name__ == '__main__':
     app.run(debug=True)
