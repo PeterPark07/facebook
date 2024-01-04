05e25b1a4d3b5d52e3d0def915b989ecb7e6c3e4
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-04 23:18:21 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,10 +20,44 @@ def commands(text):
             num = 5
         elif not parts[1].isdigit():
             return text, effects
+
+Sure, you can use delete_one multiple times to achieve the same result. Here's the modified code:
+
+python
+Copy code
+from database import messages_collection
+
+def commands(text):
+    effects = {}
+    
+    if '/persist' in text:
+        text = text.replace('/persist', '')
+        effects['persist'] = True
+        
+    if '/golden' in text:
+        text = text.replace('/golden', '')
+        effects['golden'] = True
+
+    if '/animate' in text:
+        text = text.replace('/animate', '')
+        effects['animate'] = True
+
+    if text.startswith('/delete') and len(text.split()) < 3:
+        parts = text.split()
+        
+        if len(parts) == 1:
+            num = 5
+        elif not parts[1].isdigit():
+            return text, effects
         else:
             num = int(parts[1])
-            
-        from database import messages_collection
-        messages_collection.delete_many({'persist': {'$exists': False}}, sort=[("_id", -1)], limit=num)
-    
+            if num > 20:
+                num = 20
+        
+        # Get the messages to be deleted, sorted by _id in descending order
+        messages_to_delete = messages_collection.find().sort("_id", -1).limit(num)
+        
+        # Delete the messages one by one
+        for message in messages_to_delete:
+            messages_collection.delete_one({"_id": message["_id"]})
     return text, effects
