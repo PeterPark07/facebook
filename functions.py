9162da8f643b85ff98a54e76d4aab897001282ed
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-04 23:06:15 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,5 +8,21 @@ def commands(text):
     if '/golden' in text:
         text = text.replace('/golden', '')
         effects['golden'] = True
-        
+
+    if '/animate' in text:
+        text = text.replace('/animate', '')
+        effects['animate'] = True
+
+    if text.startswith('/delete') and len(text.split()) < 3:
+        if len(text.split()) == 1:
+            num = 5
+            
+        elif not text.split()[1].isdigit():
+            return text, effects
+            
+        elif text.split()[1].isdigit():
+            num = int(text.split()[1])
+            
+        from database import messages_collection
+        messages_collection.delete_many({'persist': {'$exists': False}}, sort=[("_id", -1)], limit=num)
     return text, effects
