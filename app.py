abe9c8d3bf404c9a8b83f89dd5346c1261510f43
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-05-28 04:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     messages = list(messages_collection.find())
-    last_date = None
+    last_date = ''
 
     for message in messages:
         system = message.get('system')
