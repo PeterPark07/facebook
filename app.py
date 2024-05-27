6e924af59c73198cf11d90dd4194548b5a3d7c4a
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-05-28 03:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-import logging
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, emit
+#from flask_socketio import SocketIO, emit
 from datetime import datetime
 import pytz
 from database import messages_collection, user_log
 from functions import commands
-from bson import ObjectId
 
 app = Flask(__name__)
-socketio = SocketIO(app, logger=True, engineio_logger=True)
-
-logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -20,106 +15,150 @@ def index():
     for message in messages:
         system = message.get('system')
         if system:
-            message_date = message.get('display_time')[:10]
+            message_date = message.get('display_time')[:10]  # Extract date part
             message['display_time'] = message.get('display_time')[11:]
+
         else:
             message_date = message.get('timestamp')[:10]
+
             if message_date != last_date:
+                # Add a key to indicate a new date divider should be shown before this message
                 message['new_date'] = message_date
                 last_date = message_date
 
-        # Ensure all ObjectId fields are converted to strings
-        if '_id' in message:
-            message['_id'] = str(message['_id'])
-
     return render_template('index.html', messages=messages)
 
-@socketio.on('send_message')
-def handle_send_message(data):
-    try:
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    # Get user input
+    username = request.form.get('username')
+    user_message = request.form.get('message')
+    effects = {}
+    
+    if username and user_message:
+        if '/' in user_message:
+            user_message, effects = commands(user_message)
+        
+        # Store the message in the database
+        # Localize the timestamp to Indian timezone
+        timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
+        display_time = timestamp.strftime('%H:%M')
+        
+        new_message = {
+            'username': username,
+            'message': user_message,
+            'display_time': display_time,
+            'timestamp': timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+        new_message.update(effects)
+        
+        messages_collection.insert_one(new_message)
+        
+        return jsonify({'success': True})
+
+    return jsonify({'success': False, 'error': 'Username and message are required.'})
+
+
+@app.route('/log-user-info', methods=['POST'])
+def log_user_info():
+    data = request.get_json()
+
+    if 'username' in data and 'userInfo' in data:
         username = data['username']
-        user_message = data['message']
-        effects = {}
+        user_info = data['userInfo']
 
-        if username and user_message:
-            if '/' in user_message:
-                user_message, effects = commands(user_message)
+        # Add additional information to the user_info dictionary
+        user_info.update({
+            'Username': username,
+            'Timestamp': datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M")
+        })
 
-            timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
-            display_time = timestamp.strftime('%H:%M')
+        # Check if a record with the same username and timestamp already exists
+        existing_record = user_log.find_one(user_info)
 
-            new_message = {
-                'username': username,
-                'message': user_message,
-                'display_time': display_time,
-                'timestamp': timestamp.strftime("%Y-%m-%d %H:%M:%S")
-            }
+        if existing_record:
+            # Update the existing record instead of inserting a new one
+            user_log.update_one({'_id': existing_record['_id']}, {'$set': user_info})
+        else:
+            # Insert a new record
+            user_log.insert_one(user_info)
 
-            new_message.update(effects)
-            result = messages_collection.insert_one(new_message)
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False, 'error': 'Invalid request data.'})
 
-            # Add the inserted_id to the message for sending to the client
-            new_message['_id'] = str(result.inserted_id)
 
-            emit('receive_message', new_message, broadcast=True)
-    except Exception as e:
-        app.logger.error(f"Error in handle_send_message: {str(e)}", exc_info=True)
-
-@socketio.on('user_entered')
-def handle_user_entered(data):
-    try:
-        entered_username = data['enteredUsername']
-        if entered_username:
-            timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
-
-            entry_message = {
-                'username': 'System',
-                'message': f'{entered_username} has entered the chat!',
-                'display_time': timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-                'system': True,
-            }
-
-            result = messages_collection.insert_one(entry_message)
-            entry_message['_id'] = str(result.inserted_id)
-
-            emit('receive_message', entry_message, broadcast=True)
-    except Exception as e:
-        app.logger.error(f"Error in handle_user_entered: {str(e)}", exc_info=True)
-
-@socketio.on('user_left')
-def handle_user_left(data):
-    try:
-        username = data['username']
-        if username:
-            system_message = f'{username} has left the chat.'
-            timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
-
-            new_message = {
-                'username': 'System',
-                'message': system_message,
-                'display_time': timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-                'system': True
-            }
-
-            result = messages_collection.insert_one(new_message)
-            new_message['_id'] = str(result.inserted_id)
-
-            emit('receive_message', new_message, broadcast=True)
-    except Exception as e:
-        app.logger.error(f"Error in handle_user_left: {str(e)}", exc_info=True)
 
 
 @app.route('/delete-chats', methods=['POST'])
 def delete_chats():
+    # Get the password for authentication
     password = request.form.get('pin')
+
+    # Implement your password validation logic here
+    # For simplicity, let's assume the password is 'secret'
     if password == '0':
+        # Delete messages without persistence flag
         messages_collection.delete_many({'persist': {'$exists': False}})
         return jsonify({'success': True})
+        
     elif password == '1234':
+        # Delete all messages
         messages_collection.delete_many({})
         return jsonify({'success': True})
+        
     else:
         return jsonify({'success': False, 'error': 'Invalid password.'})
 
+
+@app.route('/user-entered', methods=['POST'])
+def user_entered():
+    entered_username = request.form.get('enteredUsername')
+    
+    if entered_username:
+        timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
+        display_time = timestamp.strftime('%H:%M')
+
+        # Create a system notification message for the user entry
+        entry_message = {
+            'username': 'System',
+            'message': f'{entered_username} has entered the chat!',
+            'display_time': timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            'system': True,
+        }
+
+        messages_collection.insert_one(entry_message)
+        
+        return jsonify({'success': True})
+
+    return jsonify({'success': False, 'error': 'Entered username not provided.'})
+
+@app.route('/exit', methods=['POST'])
+def handle_exit():
+    # Get user input
+    username = request.form.get('username')
+
+    if username:
+        # Create a system message indicating that the user has left the chat
+        system_message = f'{username} has left the chat.'
+        timestamp = datetime.now(pytz.timezone("Asia/Kolkata"))
+        display_time = timestamp.strftime('%H:%M')
+
+        new_message = {
+            'username': 'System',
+            'message': system_message,
+            'display_time': timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            'system': True
+        }
+
+        messages_collection.insert_one(new_message)
+
+        return jsonify({'success': True})
+
+    return jsonify({'success': False, 'error': 'Username is required.'})
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
