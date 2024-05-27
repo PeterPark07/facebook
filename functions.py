040fcd71a6f4cd514287c6f44d47f14d2a0c7a59
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-05-28 04:03:01 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,6 +2,25 @@ from database import messages_collection
 
 def commands(text):
     effects = {}
+    
+    if '/persist' in text:
+        text = text.replace('/persist', '')
+        effects['persist'] = True
+        
+    if '/golden' in text:
+        text = text.replace('/golden', '')
+        effects['golden'] = True
+
+    if '/animate' in text:
+        text = text.replace('/animate', '')
+        effects['animate'] = True
+
+    if '/highlight' in text:
+        text = text.replace('/highlight', '')
+        effects['highlight'] = True
+        effects['golden'] = False
+        effects['animate'] = False
+    
     if text.startswith('/delete') and len(text.split()) < 3:
         parts = text.split()
         
